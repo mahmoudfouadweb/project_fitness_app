88d0866e9e1d58cf8bd1129d22588b368caa58a1
mahmoudfouadweb <mfapolice@gmail.com>
2022-10-29 00:23:18 +0200

    init App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
