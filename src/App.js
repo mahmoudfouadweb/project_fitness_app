74d71334cad2f6bd3e41d7c1bd3c04da6bbd5feb
mahmoudfouadweb <mfapolice@gmail.com>
2022-10-29 00:58:48 +0200

    d

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-      {/* fe */}
       <Footer />
     </Box>
   );
