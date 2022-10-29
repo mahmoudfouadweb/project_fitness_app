82aaa1b437efd97555385878bff3bfc58765272c
mahmoudfouadweb <mfapolice@gmail.com>
2022-10-29 05:22:52 +0200

    add components for home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,16 @@ const Navbar = () => {
       direction={'row'}
       justifyContent="space-around"
       sx={{
-        gap,
+        gap: {
+          sm: '122px',
+          xs: '40px',
+        },
+        mt: {
+          sm: '32px',
+          xs: '20px',
+        },
+        justifyContent: 'none',
+        px: '20px',
       }}
     >
       <Link to="/">
