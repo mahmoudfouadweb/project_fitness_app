16369a75181c2566361c5168ae54b72196168e5f
mahmoudfouadweb <mfapolice@gmail.com>
2022-10-29 05:06:20 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,52 @@
+import { Stack } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction={'row'}
+      justifyContent="space-around"
+      sx={{
+        gap,
+      }}
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
+      </Link>
+
+      <Stack
+        direction="row"
+        gap={'40px'}
+        fontSize="24px"
+        alignItems={'flex-end'}
+      >
+        <Link
+          to={'/'}
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #ff2625',
+          }}
+        >
+          {' '}
+          Home{' '}
+        </Link>
+        <a
+          href="#exercise"
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Exercise
+        </a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
